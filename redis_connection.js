dd4b3a8e10e3cc03eec9eabb662b712827b2a0c2
yuvalfayna <yovalfayna98@gmail.com>
2024-10-25 03:34:27 +0300

    bugfix

--- redis_connection.js
+++ redis_connection.js
@@ -37,12 +37,12 @@ client.on('error', (err) => console.log('Redis Client Error', err));
 
 app.get('/data', async (req, res) => {
     try {
-        const keys = await client.keys("*"+userIp+"#random*");
+        const keys = await client.keys(userIp+"#random*");
         const jdata = await Promise.all(keys.map((id) => client.get(id)));
         const data = await Promise.all(jdata.map(item => JSON.parse(item)[0]));
         const arrdata= await client.get(userIp+"#dataarr");
         const jarrdata= await JSON.parse(arrdata);
-        res.json({ data,jarrdata });
+        res.json({ data, jarrdata });
     } catch (err) {
         console.error('Error fetching from Redis:', err);
         res.status(500).send('Server error');
@@ -51,7 +51,7 @@ app.get('/data', async (req, res) => {
 
 app.get('/datamap', async (req, res) => {
     try {
-        const keys = await client.keys("*"+userIp+"#maprandom*");
+        const keys = await client.keys(userIp+"#maprandom*");
         const jdata = await Promise.all(keys.map((id) => client.get(id)));
         const data = await Promise.all(jdata.map(item => JSON.parse(item)[0]));
         const arrdata= await client.get(userIp+"#dataarrmap");
